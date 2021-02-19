87054b6653b53ce0753e4b95e4385e412c068d14
ik18015 <79236404+ik18015@users.noreply.github.com>
2021-02-19 13:37:50 +0900

    FizzBuzzできたよ

---
+++ src/FizzBuzz.java
@@ -0,0 +1,23 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+//            System.out.print("joc");
+//            System.out.println(i);
+//            System.out.println(i % 3);
+//            System.out.println(i % 5);
+//            System.out.println(i % 3 == 0 && i % 5 == 0);
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
